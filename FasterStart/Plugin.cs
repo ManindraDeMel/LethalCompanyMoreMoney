b69dbccde7063ef9518b5fdb82a4001a677f3286
Manindra de Mel <u7156805@anu.edu.au>
2023-12-01 18:33:07 +1100

    Add project files.

---
+++ FasterStart/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasterStart
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class FasterStart : BaseUnityPlugin
+    {
+        private const string modGUID = "299792458.FasterStart";
+        private const string modName = "Faster Start";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static FasterStart Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Test");
+
+            harmony.PatchAll(typeof(FasterStart));
+        }
+    }
+}
